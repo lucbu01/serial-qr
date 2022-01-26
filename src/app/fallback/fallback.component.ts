e678e33ad00c61bb3663345ae712b93712a9e646
Luca Bucher <lucbu01@bluewin.ch>
2022-01-26 11:09:34 +0100

    optimize fallback component

--- src/app/fallback/fallback.component.ts
+++ src/app/fallback/fallback.component.ts
@@ -7,12 +7,15 @@ import { ProjectService } from '../services/project.service';
   styleUrls: ['./fallback.component.scss']
 })
 export class FallbackComponent implements OnInit {
+  loading = true;
   constructor(private projectService: ProjectService) {}
 
   ngOnInit(): void {
     document.title = 'SerialQR - Serienbriefe mit QR-Rechnung';
     if (localStorage.getItem('serialqr.lastviewed')) {
       this.projectService.openLastViewed();
+    } else {
+      this.loading = false;
     }
   }
 
